c9f5c14179edead63d153b0155e7d9ed33e4036c
aphsol <hasagiasuo835@gmail.com>
2024-09-11 13:50:59 +0300

    1

--- src/loginFrame.cpp
+++ src/loginFrame.cpp
@@ -13,9 +13,12 @@ LoginFrame::LoginFrame() : wxFrame(nullptr, wxID_ANY, "Login", wxDefaultPosition
   this->bg_image = new wxBitmap(bg_img);
   wxStaticText* login_l = new wxStaticText(this, wxID_ANY, "Email", wxPoint(32, 30));
   this->login = new wxTextCtrl(this, wxID_ANY, wxEmptyString, wxPoint(120, 30), wxSize(150, 25), wxTE_CENTRE | wxTE_NO_VSCROLL);
+  this->login->SetBackgroundColour(*wxBLACK);
   wxStaticText* password_l = new wxStaticText(this, wxID_ANY, "Password", wxPoint(22, 80));
   this->password = new wxTextCtrl(this, wxID_ANY, wxEmptyString, wxPoint(120, 80), wxSize(150, 25), wxTE_PASSWORD | wxTE_CENTRE | wxTE_NO_VSCROLL);
+  this->password->SetBackgroundColour(*wxBLACK);
   wxButton* btn_commit = new wxButton(this, wxID_ANY, "Login", wxPoint(100, 110), wxSize(100, 30));
+  btn_commit->SetBackgroundColour(*wxBLACK);
   Bind(wxEVT_PAINT, &LoginFrame::draw_bg, this);
 }
 
